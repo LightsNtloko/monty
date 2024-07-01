1e224ded55b8c72f9cbe95362eb2d37a5435d4a3
LightsNtloko <ST10125711@rcconnect.edu.za>
2024-07-01 07:01:21 -0700

    The opcode pop removes the top element of the stack

--- pop.c
+++ pop.c
@@ -11,7 +11,7 @@ void pop(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pop an empty stack\\n",
+		fprintf(stderr, "L%d: can't pop an empty stack\n",
 				line_number);
 		exit(EXIT_FAILURE);
 	}

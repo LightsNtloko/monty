b0a4d5171001ab490571b7ed721776b2d91b7841
LightsNtloko <ST10125711@rcconnect.edu.za>
2024-07-02 13:16:53 -0700

    The opcode mod computes the rest of the division of the second top element of the stack by the top element of the stack

---
+++ mod.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * mod - The fuction that computes the rest of the division of the secod
+ * top element of the stack by the top element of the stack.
+ * @stack: The double pointer to the top of the stack
+ * @line_number: The current line number in the bytecode file.
+ */
+void mod(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = *stack;
+	(*stack)->next->n %= (*stack)->n;
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+	free(temp);
+}
